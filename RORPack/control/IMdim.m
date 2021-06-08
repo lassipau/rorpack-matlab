c5b0da7e1505cc19889cfd6edfd10bbc8e3c9cab
Lassi Paunonen (TAU) <paunonel@wks-94982-mac.ad.tuni.fi>
2021-06-08 18:28:58 +0300

    Added routine "IMdim" for computing dim(Z0)

---
+++ RORPack/control/IMdim.m
@@ -0,0 +1,17 @@
+function dimZ0 = IMdim(freqs,p)
+% function dimZ0 = IMdim(freqs,p)
+%
+% Compute the dimension of the internal model. 
+% Parameters: 
+% 'freqs' =  the (real) frequencies of the internal model. If 0 is a 
+% frequency, it is required to be the first element of the vector.
+% 'p' = number of outputs of the system
+%
+% 'dimZ0' = dimension of the internal model.
+
+if freqs(1) == 0
+    dimZ0 = (2*length(freqs)-1)*p;
+else
+    dimZ0 = 2*length(freqs)*p;
+end
+    
