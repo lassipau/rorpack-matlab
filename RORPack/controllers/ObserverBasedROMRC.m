2c0892eac54b0b9b01719fe5742c07774869e0c1
Lassi Paunonen (TAU) <paunonel@wks-94982-mac.ad.tuni.fi>
2021-07-01 12:31:58 +0300

    Improved ROM controller performance, added error to CLStabmarg
    Improved ROM controller performance by changing from 'are' to 'icare'
    (automatically using 'care' for Matlab versions earlier than R2019a).
    Also added consistency checks for parameters.
    
    In CLStabmarg, added a second parameter to make it possible to raise an
    error (instead of a warning) if CL system is unstable.

--- RORPack/controllers/ObserverBasedROMRC.m
+++ RORPack/controllers/ObserverBasedROMRC.m
@@ -1,19 +1,20 @@
 function ContrSys = ObserverBasedROMRC(freqsReal,SysApprox,alpha1,alpha2,R1,R2,Q0,Q1,Q2,ROMorder)
 % ContrSys = ObserverBasedROMRC(freqs,Pvals,Sys)
 %
-% Construct a reduced order observer-based robust controller for systems with the same number of 
-% inputs and outputs. The frequencies are assumed to be conjugate pairs, and the internal 
+% Construct a reduced order observer-based robust controller for systems with the same number of
+% inputs and outputs. The frequencies are assumed to be conjugate pairs, and the internal
 % model is in real form
 % freqsReal = Frequencies to be included in the controller, only real nonnegative
 % frequencies, if zero frequency is included, it's the first element in the
 % vector. The control system is assumed to be real (i.e.,
 % P(conj(s))=conj(P(s))), and P(iw_k) are invertible at the frequencies of
 % the reference and disturbance signals.
-% SysApprox = The Galerkin approximation (A^N,B^N,C^N,D) of the control 
+% SysApprox = The Galerkin approximation (A^N,B^N,C^N,D) of the control
 % system for the controller design. This is a struct with fields
 % SysApprox.AN, SysApprox.BN, SysApprox.CN, and SysApprox.D
 % IMstabmarg = intended stability margin of the closed-loop system
-% ROMorder = order of the reduced-order observer in the controller
+% ROMorder = order of the reduced-order observer in the controller. The
+%            model reduction step can be skipped by setting 'ROMorder=NaN'
 %
 % ContrSys = Controller parameters (ContrSys.G1,ContrSys.G2,ContrSys.K)
 %
@@ -32,76 +33,170 @@ dimX = size(AN,1);
 dimY = size(CN,1);
 dimU = size(BN,2);
 
-if dimY ~= dimU
-  error('The has an unequal number of inputs and outputs, the observer-based controller design cannot be completed.')
+% Check the consistency of the controller design parameters
+
+alpha1_check = isreal(alpha1) && isscalar(alpha1) && alpha1>0;
+alpha2_check = isreal(alpha2) && isscalar(alpha2) && alpha2>0;
+
+R1_check = ishermitian(R1) && all(eig(R1)>0);
+R2_check = ishermitian(R2) && all(eig(R2)>0);
+
+if ~alpha1_check || ~alpha2_check
+    error('"alpha1" and "alpha2" need to be positive!')
+elseif ~R1_check || ~R2_check
+    error('"R1" and "R2" need to be positive definite')
 end
-
-% q = length(freqs);
-
-
-% if freqs(1)==0, dimZ = dimY*(2*q-1); else dimZ = dimY*2*q; end
-
-% B1 = zeros(dimZ,dimY);
-% if freqs(1)==0
-%   offset = 1;
-%   B1(1:dimY,:) = PKvals;
-% end
-% 
-% 
-% PKappr = @(s) (C+D*K21)*((s*eye(dimX)-(A+B*K21))\B)+D;
-% for ind = 1:q
-%   if cond(PKappr(1i*freqs(ind)))>1e6
-%     warning(['The matrix P_K(iw_k) for k=' num2str(ind) ' is nearly singular!'])
-%   end
-% end
-
 
 % Construct the internal model
 [G1,G2] = ConstrIM(freqsReal,dimY);
+dimZ0 = size(G1,1);
 
-dimZ = size(G1,1);
 
-As = [G1,G2*CN;zeros(dimX,dimZ),AN];
+% If R1, R2, Q0, Q1, or Q2 have scalar values, these are interpreted as
+% "scalar times identity".
+if dimY>1 && isscalar(R1)
+    R1 = R1*eye(dimY);
+end
+if dimY>1 && isscalar(R2)
+    R2 = R2*eye(dimY);
+end
+if dimZ0>1 && isscalar(Q0)
+    Q0 = Q0*eye(dimZ0);
+end
+if dimX>1 && isscalar(Q1)
+    Q1 = Q1*eye(dimX);
+end
+if dimX>1 && isscalar(Q2)
+    Q2 = Q2*eye(dimX);
+end
+
+
+% Check the consistency of the dimensions of R1, R2, Q0, Q1, and Q2
+if ~isequal(size(R1),[dimY,dimY])
+    error('Dimensions of "R1" are incorrect! (should be [dimY,dimY]).')
+end
+if ~isequal(size(R2),[dimU,dimU])
+    error('Dimensions of "R1" are incorrect! (should be [dimU,dimU]).')
+end
+if ~isequal(size(Q0,2),dimZ0)
+    error('Dimensions of "Q0" are incorrect!')
+end
+if ~isequal(size(Q1,1),dimX)
+    error('Dimensions of "Q1" are incorrect!')
+end
+if ~isequal(size(Q2,2),dimX)
+    error('Dimensions of "Q2" are incorrect!')
+end
+
+
+% Form the extended system (As,Bs)
+As = [G1,G2*CN;zeros(dimX,dimZ0),AN];
 Bs = [G2*D;BN];
 
 Qs = blkdiag(Q0,Q2);
 
-SigmaN = are((AN+alpha1*eye(dimX))',CN'*(R1\CN),Q1*Q1');
-PiN = are(As+alpha2*eye(dimZ+dimX),Bs*(R2\Bs'),Qs'*Qs);
 
-L = -SigmaN*(CN'/R1);
-K = - (R2\Bs')*PiN;
-K1N = K(:,1:dimZ);
-K2N = K(:,(dimZ+1):end);
+% Stabilize the pairs (CN,AN+alpha1) and (As+alpha2,Bs) using LQR/LQG design
+if verLessThan('matlab','R2019a')
+    
+    warning('Matlab version prior to R2019a: Using "care" instead of "icare" (may be less accurate or slower).')
+    
+    % Stabilize the pair (CN,AN+alpha1)
+    tic
+    B_ext = [CN',zeros(dimX)];
+    S_ext = [zeros(dimX,dimY),Q1];
+    R1_ext = blkdiag(R1,-eye(dimX));
+    [~,evals,L_ext_adj] = care((AN+alpha1*eye(dimX))',B_ext,0,R1_ext,S_ext);
+    fprintf(['Stabilization of the pair (CN,AN+alpha1) took ' num2str(round(toc,1)) ' seconds.\n'])
+    L = -L_ext_adj(1:dimY,:)';
+    
+    if max(real(evals))>= 0
+        error('Stabilization of the pair (CN,AN+alpha1) failed!')
+    end
+    
+    % Stabilize the pair (As+alpha2,Bs)
+    tic
+    Bs_ext = [Bs,zeros(dimZ0+dimX)];
+    Ss_ext = [zeros(dimZ0+dimX,dimY),Qs'];
+    R2_ext = blkdiag(R2,-eye(dimZ0+dimX));
+    [~,evals,K_ext] = care(As+alpha2*eye(dimZ0+dimX),Bs_ext,0,R2_ext,Ss_ext);
+    K = -K_ext(1:dimU,:);
+    fprintf(['Stabilization of the pair (As+alpha2,Bs) took ' num2str(round(toc,1)) ' seconds.\n'])
+    
+    if max(real(evals))>= 0
+        error('Stabilization of the pair (As+alpha2,Bs) failed!')
+    end
+    
+else
+    
+    % Stabilize the pair (CN,AN+alpha1)
+    tic
+    B_ext = [CN',zeros(dimX)];
+    S_ext = [zeros(dimX,dimY),Q1];
+    R1_ext = blkdiag(R1,-eye(dimX));
+    [~,L_ext_adj,evals] = icare((AN+alpha1*eye(dimX))',B_ext,0,R1_ext,S_ext,[],[]);
+    L = -L_ext_adj(1:dimY,:)';
+    fprintf(['Stabilization of the pair (CN,AN+alpha1) took ' num2str(round(toc,1)) ' seconds.\n'])
+    
+    if max(real(evals))>= 0
+        error('Stabilization of the closed-loop system failed!')
+    end
+    
+    % Stabilize the pair (As+alpha2,Bs)
+    tic
+    Bs_ext = [Bs,zeros(dimZ0+dimX)];
+    Ss_ext = [zeros(dimZ0+dimX,dimY),Qs'];
+    R2_ext = blkdiag(R2,-eye(dimZ0+dimX));
+    [~,K_ext,evals] = icare(As+alpha2*eye(dimZ0+dimX),Bs_ext,0,R2_ext,Ss_ext,[],[]);
+    K = -K_ext(1:dimU,:);
+    fprintf(['Stabilization of the pair (As+alpha2,Bs) took ' num2str(round(toc,1)) ' seconds.\n'])
+    
+    if max(real(evals))>= 0
+        error('Stabilization of the pair (As+alpha2,Bs) failed!')
+    end
+    
+    
+end
 
-rsys = balred(ss(AN+L*CN,[BN+L*D,L],K2N,zeros(dimU,dimU+dimY)),ROMorder);
-ALr = rsys.A;
-Br_full = rsys.B;
-BLr = Br_full(:,1:dimU);
-Lr = Br_full(:,(dimU+1):end);
-K2r = rsys.C;
+% Decompose the control gain K into K=[K1N,K2N]
+K1N = K(:,1:dimZ0);
+K2N = K(:,(dimZ0+1):end);
 
-% % Debug: Omit model reduction
-% ALr = A+L*C;
-% BLr = B+L*D;
-% Lr = L;
-% K2r = K2N;
-% ROMorder = dimX;
 
-ContrSys.G1 = [G1,zeros(dimZ,ROMorder);BLr*K1N,ALr+BLr*K2r];
+% Complete the model reduction step of the controller design. If the model
+% reduction fails (for example due to too high reduction order), the
+% controller design is by default completed without the model reduction.
+if ~isnan(ROMorder)
+    try
+        tic
+        rsys = balred(ss(AN+L*CN,[BN+L*D,L],K2N,zeros(dimU,dimU+dimY)),ROMorder);
+        fprintf(['Model reduction step took ' num2str(round(toc,1)) ' seconds.\n'])
+        
+        ALr = rsys.A;
+        Br_full = rsys.B;
+        BLr = Br_full(:,1:dimU);
+        Lr = Br_full(:,(dimU+1):end);
+        K2r = rsys.C;
+    catch
+        warning(['Model reduction step failed! Modify "ROMorder", or check that "balred" is available. ', ...
+            'Proceeding without model reduction (with option "ROMorder=NaN")']);
+    end
+end
+
+if isnan(ROMorder)
+    fprintf('Constructing the controller without model reduction.\n')
+    
+    ALr = AN+L*CN;
+    BLr = BN+L*D;
+    Lr = L;
+    K2r = K2N;
+end
+
+
+% Construct the controller matrices (G1,G2,K).
+ContrSys.G1 = [G1,zeros(dimZ0,size(ALr,1));BLr*K1N,ALr+BLr*K2r];
 ContrSys.G2 = [G2;-Lr];
 ContrSys.K = [K1N, K2r];
-
-
-
-% % Temp
-% % Check validity of the controller!
-% 
-% Gf1 = ContrSys.G1;
-% Gf2 = ContrSys.G2;
-% Kf = ContrSys.K;
-% Ae = [A B*Kf;Gf2*C Gf1+Gf2*D*Kf];
-% Qe = [-eye(dimX) zeros(dimX,size(Gf1,2));H zeros(size(G1,1),size(Gf1,2)); -eye(dimX) zeros(dimX,size(G1,1)) eye(dimX)];
 
 
 
