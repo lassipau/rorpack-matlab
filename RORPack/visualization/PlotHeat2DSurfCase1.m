510f0141ecd094aba8cf514b7c03639ec84978d3
Jonne Karru (TAU) <jonne.karru@tuni.fi>
2021-07-07 12:17:04 +0300

    Add working surface plot at time t to Heat2DCase1.

--- RORPack/visualization/PlotHeat2DSurfCase1.m
+++ RORPack/visualization/PlotHeat2DSurfCase1.m
@@ -1,10 +1,9 @@
 function PlotHeat2DSurfCase1(CLsim,spgrid,tgrid)
-% function PlotHeat2DSurf(state,spgrid)
 %
 % Plot the solution of the 2D Heat equation
-% state = state of the closed-loop system 
-% zlims = limits for the z-axis (optional)
-
+% 
+% 
+% 
 
 xx = spgrid.xx;
 yy = spgrid.yy;
@@ -25,10 +24,12 @@ zz = real(zz);
 zlims(1) = min(min(min(zz)));
 zlims(2) = max(max(max(zz)));
 axlims = [xx(1,1) xx(1,end) yy(1,1) yy(end,1) zlims];
-    
-surf(xx,yy,reshape(zz(:,150),N,N));
-% set(h,'linewidth',0.5,'edgecolor',0.5*[1 1 1])
+
+% state at time ind
+ind = 150;
+surf(xx,yy,reshape(zz(:,ind),N,N));
 axis(axlims)
 caxis(zlims)
 ylabel('$y$','Interpreter','latex','Fontsize',20)
 xlabel('$x$','Interpreter','latex','Fontsize',20)
+title(['The state of the system at $t=' num2str(round(tgrid(ind),1)) '$.'],'interpreter', 'latex', 'fontsize', 20);
