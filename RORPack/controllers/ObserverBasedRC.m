20686744053db9e71f73c88d71b2a1ac8e0402be
Lassi Paunonen (TAU) <paunonel@wks-94982-mac.ad.tuni.fi>
2022-03-05 14:03:25 +0200

    Added a new stabilization type 'full_K' to ObserverBasedRC

--- RORPack/controllers/ObserverBasedRC.m
+++ RORPack/controllers/ObserverBasedRC.m
@@ -16,13 +16,15 @@ function [ContrSys,K21] = ObserverBasedRC(freqsReal,Sys,K21,L,IMstabtype,IMstabm
 %   Sys : [struct with fields A,B,C,D] The controlled system
 %
 %   K21 : [M1xN1 matrix] The matrix K21 which should be chosen
-%   so that A + B * K21 is stable.
+%   so that A + B * K21 is stable. If 'IMstabtype' is 'full_K', then this
+%   parameter is instead an operator K chosen so that A_s + B_s * K is
+%   exponentially stable, where A_s = [G1,G2*C;0,G1] and B_s=[G2*D;B].
 %
 %   L : [M2xN2 matrix] The matrix L which should be chosen
 %   so that A + L * C is stable.
 %
 %   IMstabtype : [string] Stabilization of the internal model
-%   using either 'LQR' or 'poleplacement'
+%   using either 'LQR', 'poleplacement', or 'full_K'.
 %
 %   IMstabmarg : [double] The desired stability margin
 %   for the internal model.
@@ -40,56 +42,72 @@ dimX = size(A,1);
 dimY = size(C,1);
 dimU = size(B,2);
 
-if dimY ~= dimU
-  error('The system has an unequal number of inputs and outputs, the observer-based controller design cannot be completed (in this form).')
-end
-
 q = length(freqsReal);
 
 
-PKappr = @(s) (C+D*K21)*((s*eye(dimX)-(A+B*K21))\B)+D;
-for ind = 1:q
-  if cond(PKappr(1i*freqsReal(ind)))>1e6
-    warning(['The matrix P_K(iw_k) for k=' num2str(ind) ' is nearly singular!'])
-  end
+if ~isequal(IMstabtype,'full_K')
+    PKappr = @(s) (C+D*K21)*((s*eye(dimX)-(A+B*K21))\B)+D;
+    for ind = 1:q
+        if cond(PKappr(1i*freqsReal(ind)))>1e6
+            warning(['The matrix P_K(iw_k) for k=' num2str(ind) ' is nearly singular!'])
+        end
+    end
 end
-
 
 % Construct the internal model
 [G1,G2] = ConstrIM(freqsReal,dimY);
 
 dimZ = size(G1,1);
 
-% Find H as the solution of G1*H=H*(A+B*K21)+G2*(C+D*K21) and define B1
-H = sylvester(G1,-(A+B*K21),G2*(C+D*K21));
-B1 = H*B+G2*D;
-
-% Stabilization of the internal model, choose K1 so that G1+B1*K1 is
-% exponentially stable
-if isequal(IMstabtype,'LQR')
-  K1 = -lqr(G1+IMstabmarg*eye(dimZ),B1,100*eye(dimZ),0.001*eye(dimU),zeros(dimZ,dimU));
-elseif isequal(IMstabtype,'poleplacement')
-  if freqsReal(1)==0
-    target_eigs = ones(2*length(freqsReal)-1,1)*linspace(-1.1*IMstabmarg,-IMstabmarg,dimY)...
-                    +1i*reshape([freqsReal(end:-1:1),-freqsReal(2:end)],2*length(freqsReal)-1,1)*ones(1,dimY);
-    target_eigs = target_eigs(:);
-    K1 = -place(G1,B1,target_eigs);
-  else
-    target_eigs = ones(2*length(freqsReal),1)*linspace(-1.1*IMstabmarg,-IMstabmarg,dimY)...
-                    +1i*reshape([freqsReal(end:-1:1),-freqsReal],2*length(freqsReal),1)*ones(1,dimY);
-    target_eigs = target_eigs(:);
-    K1 = -place(G1,B1,target_eigs);
-  end
+if isequal(IMstabtype,'full_K')
+    % The full operator K is given in the place of parameter "K21"
+    
+    % Check that the dimensions are correct
+    if ~isequal(size(K21),[dimU,dimZ+dimX])
+        error('The dimensions of the input parameter K21 are not correct!')
+    end
+    
+    K1 = K21(:,1:dimZ);
+    K2 = K21(:,(dimZ+1):end);
+    
 else
-  error('Unknown stabilization type for the Internal Model')
-end
+    
+    if dimY ~= dimU
+      error('The system has an unequal number of inputs and outputs, the observer-based controller design cannot be completed (in this form).')
+    end
 
-K2 = K21+K1*H;
+    % Find H as the solution of G1*H=H*(A+B*K21)+G2*(C+D*K21) and define B1
+    H = sylvester(G1,-(A+B*K21),G2*(C+D*K21));
+    B1 = H*B+G2*D;
+    
+    % Stabilization of the internal model, choose K1 so that G1+B1*K1 is
+    % exponentially stable
+    if isequal(IMstabtype,'LQR')
+        K1 = -lqr(G1+IMstabmarg*eye(dimZ),B1,100*eye(dimZ),0.001*eye(dimU),zeros(dimZ,dimU));
+        
+    elseif isequal(IMstabtype,'poleplacement')
+        if freqsReal(1)==0
+            target_eigs = ones(2*length(freqsReal)-1,1)*linspace(-1.1*IMstabmarg,-IMstabmarg,dimY)...
+                +1i*reshape([freqsReal(end:-1:1),-freqsReal(2:end)],2*length(freqsReal)-1,1)*ones(1,dimY);
+            target_eigs = target_eigs(:);
+            K1 = -place(G1,B1,target_eigs);
+        else
+            target_eigs = ones(2*length(freqsReal),1)*linspace(-1.1*IMstabmarg,-IMstabmarg,dimY)...
+                +1i*reshape([freqsReal(end:-1:1),-freqsReal],2*length(freqsReal),1)*ones(1,dimY);
+            target_eigs = target_eigs(:);
+            K1 = -place(G1,B1,target_eigs);
+        end
+    else
+        error('Unknown stabilization type for the Internal Model')
+    end
+    
+    K2 = K21+K1*H;
+end
 
 
 % IN PROGRESS: An alternative option for stabilization: If K21 not given, 
 % could use LQR to construct [K1 K2] as a stabilizing feedback gain for the 
-% pair ([A 0;G2*C G1],[B;G2*D])
+% pair ([G1,G2*C;0,G1],[G2*D;B])
 % Kfull = -lqr([G1 G2*C;zeros(dimX,dimZ) A],[G2*D;B],blkdiag(200*eye(dimZ),.01*eye(dimX)),0.01*eye(dimU),zeros(dimZ+dimX,dimU));
 % K1 = Kfull(:,1:dimZ);
 % K2 = Kfull(:,(dimZ+1):end);
